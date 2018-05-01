46935ba56e14d46ba8be2bc262a078937f360b02
MiguelRomeroGmx <contacto@miguelromero.mx>
2018-05-01 18:15:03 -0500

    componentes html

---
+++ app.js
@@ -0,0 +1,14 @@
+var config = {
+    apiKey: "AIzaSyCtObdc66qWY73dCoxeDwZV9UHHB8zQB3I",
+    authDomain: "conectodosping.firebaseapp.com",
+    databaseURL: "https://conectodosping.firebaseio.com",
+    projectId: "conectodosping",
+    storageBucket: "",
+    messagingSenderId: "829429621487"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+
+  
