7da4da9a20a74ac9a622993913a3f12651d72d23
MiguelRomeroGmx <contacto@miguelromero.mx>
2018-05-02 10:10:05 -0500

    ordenando datos

--- app2.js
+++ app2.js
@@ -21,6 +21,8 @@ var celda;
 var datos = 50;
 var datosRec;
 var datosDesc;
+var total1 = new Array();
+var total2 = new Array();;
 
 var fecha = new Date();
   var mes = fecha.getMonth() + 1;
@@ -42,62 +44,63 @@ var fecha = new Date();
 
     for (let index = 0; index < desconexiones; index++) {
         console.log(index);
-
-
-
-
-     var x = document.getElementById("tabla");
-     var fila = tabla.insertRow(index);
-
-     var celda1 = fila.insertCell(0);
-     var celda2 = fila.insertCell(1);
-     var celda3 = fila.insertCell(2);
+        // var x = document.getElementById("tabla");
+        // var fila = tabla.insertRow(index);
+        // // var celda1 = fila.insertCell(0);
+        // // var celda2 = fila.insertCell(1);
+        // var celda3 = fila.insertCell(2);
     
-     var posicion = index + 1;
+        var posicion = index + 1;
         var dato1 = firebase.database().ref().child("torre_1/desconexion/" + dia + "/" + posicion ); 
     
-    dato1.on("value", function (snaptshot) {
-       dato1 = snaptshot.val();
-       console.log(dato1);
-       
-    //    datosDesc[posicion]= dato1;
-       celda1.innerHTML = dato1; 
-    });
+        dato1.on("value", function (snaptshot) {
+            dato1 = snaptshot.val();
+            datosDesconexion(dato1, index);
+            // construirTabla1(index);
+            console.log(dato1);
+            console.log(posicion);
+            var tabla = document.getElementById("tabla");
+            var fila = tabla.insertRow(index);
+            var celda1 = fila.insertCell(0);
+            celda1.innerHTML = total1[index]; 
+        });
 
-    var dato2 = firebase.database().ref().child("torre_1/reconexion/" + dia + "/" + posicion ); 
+        var dato2 = firebase.database().ref().child("torre_1/reconexion/" + dia + "/" + posicion ); 
     
-    dato2.on("value", function (snaptshot) {
-       dato2 = snaptshot.val();
-    //    console.log(dato2);
-    //    datosRec[posicion] = dato2
-       celda2.innerHTML = dato2;
-    });
+        dato2.on("value", function (snaptshot) {
+            dato2 = snaptshot.val();
+            datosConexion(dato2, index);
+            var tabla = document.getElementById("tabla2");
+            var fila = tabla.insertRow(index);
+            var celda2 = fila.insertCell(0);
+            celda2.innerHTML = total2[index]; 
+            
+        });
+    }
 
-    
-    // console.log(dato1);
-    
-    // celda1.innerHTML = dato1; 
-    // celda2.innerHTML = dato2;
-    celda3.innerHTML = index;
-     
-
-    
-}
-
-
-
+  
 
   });
+  
+
+var datosDesconexion = function (dato1, index) {
+   console.log("datos: " + dato1);
+   console.log("index: " + index);
+   total1.push(dato1);
+   console.log(total1);
+   return;
+   
+};
 
 
-
-
-
-
-
-
-
-
+var datosConexion = function (dato2, index) {
+    console.log("datos2: " + dato2);
+    console.log("index: " + index);
+    total2.push(dato2);
+    console.log(total2);
+    
+   return;
+}
 
 
 
